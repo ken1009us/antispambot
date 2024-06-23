a9551c1914a94d2129a0d300ae50db5217f7b200
Ken <ken.wu@dimaresaas.com>
2024-06-23 16:22:21 -0700

    update model

--- src/train.py
+++ src/train.py
@@ -1,18 +1,19 @@
 import math
 
 
+from .preprocess_data import preprocess_message
+
 class NaiveBayesClassifier:
     def __init__(self, k=1.0):
         self.k = k
-        self.word_count_ = {}  # Standard dictionary to replace defaultdict
+        self.word_count_ = {}
         self.total_count_ = [0, 0]
         self.word_prob_ = {}
 
     def train(self, training_data):
-        for index, row in training_data.iterrows():
-            label = row['label']
-            words = row['message']
-            label_index = 1 if label == "spam" else 0
+        for message, label in training_data:
+            words = preprocess_message(message)
+            label_index = 1 if label == 1 else 0
             for word in words:
                 if word not in self.word_count_:
                     self.word_count_[word] = [0, 0]
@@ -20,8 +21,8 @@ class NaiveBayesClassifier:
                 self.total_count_[label_index] += 1
 
         for word, count in self.word_count_.items():
-            word_prob_if_spam = (self.k + count[1]) / (2 * self.k + self.total_count_[1])
-            word_prob_if_non_spam = (self.k + count[0]) / (2 * self.k + self.total_count_[0])
+            word_prob_if_spam = (self.k + count[1]) / (self.k * 2 + self.total_count_[1])
+            word_prob_if_non_spam = (self.k + count[0]) / (self.k * 2 + self.total_count_[0])
 
             if word not in self.word_prob_:
                 self.word_prob_[word] = [0.0, 0.0]
@@ -30,9 +31,10 @@ class NaiveBayesClassifier:
             self.word_prob_[word][0] = word_prob_if_non_spam
 
     def predict(self, message):
+        words = preprocess_message(message)
         log_prob_spam = log_prob_non_spam = 0.0
 
-        for word in message:
+        for word in words:
             if word in self.word_prob_:
                 log_prob_spam += math.log(self.word_prob_[word][1])
                 log_prob_non_spam += math.log(self.word_prob_[word][0])
@@ -43,8 +45,8 @@ class NaiveBayesClassifier:
         total_prob = prob_spam + prob_non_spam
 
         if total_prob == 0:
-            return 'ham'
+            return 0
 
         spam_probability = prob_spam / total_prob
-        return 'spam' if spam_probability > 0.5 else 'ham'
+        return 1 if spam_probability > 0.5 else 0
 
